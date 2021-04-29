78c35f66f1cf65ad2d4b3a970f9643d074688ebc
InkiInki <1964158105@qq.com>
2021-04-29 18:50:46 +0800

    Add tool by InkiInki at 20210429

---
+++ FunctionTool.py
@@ -0,0 +1,30 @@
+"""
+@author: Inki
+@contact: inki.yinji@qq.com
+@version: Created in 2020 0903, last modified in 2020 1231.
+@note: Some common function, and all given vector data's type must be numpy.array.
+"""
+
+import numpy as np
+import scipy.io as scio
+
+
+def load_file(para_path):
+    """
+    Load file.
+    :param
+        para_file_name:
+            The path of the given file.
+    :return
+        The data.
+    """
+    temp_type = para_path.split('.')[-1]
+
+    if temp_type == 'mat':
+        ret_data = scio.loadmat(para_path)
+        return ret_data['data']
+    else:
+        with open(para_path) as temp_fd:
+            ret_data = temp_fd.readlines()
+
+        return ret_data
